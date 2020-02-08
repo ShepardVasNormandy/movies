a9a94f33987b953ca0550c6746652ece99232fbb
Enzo de La Rochebrochard <enzo.delarochebrochard@ieva.io>
2020-02-08 14:54:50 +0100

    Added createMovie, deleteCollection, updateMovie

--- api/controllers/movieListController.js
+++ api/controllers/movieListController.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose')
 
 const Movie = mongoose.model('Movie');
+const rp = require('request-promise');
 
 const listAllMovies = (req, res) => {
     Movie.find({}).lean().then(movie => {
@@ -12,21 +13,34 @@ const listAllMovies = (req, res) => {
     })
 }
 const updateMovie = (req, res) => {
-    Movie.findOneAndUpdate({_id: req.params.id}, req.body, {new:true}).lean().then(movie => {
+    Movie.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).lean().then(movie => {
         res.json(movie)
     }).catch(err => {
         resizeBy.send(err)
     })
 }
-const createMovie = (req, res) => {
-    var movie = new Movie({name:req.body.name})
-    movie.save((result) => {
-        res.json(result)
+const createMovies = (req, res) => {
+    rp.get(' http://www.omdbapi.com/?s=wars&type=movie&apikey=c1d4f942').then(movies => {
+        JSON.parse(movies).Search.forEach((item, index) => {
+            item['_id'] = item.imdbID
+            let movie = new Movie(item)
+            movie.save((result) => {
+                console.warn(result)
+            })
+        })
+    })
+}
+
+const deleteCollection = (req, res) => {
+    let collectionName = req.params.collectionName
+    Movie.deleteMany({'status':'pending'}, () => {
+        res.send(200, `Collection ${collectionName} has been deleted.`)
     })
 }
 
 module.exports = {
     listAllMovies,
     updateMovie,
-    createMovie
+    createMovies,
+    deleteCollection
 }

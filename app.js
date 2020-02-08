8ff7b705ec394a26f4d099d5f47ab214b7b28bf1
Enzo de La Rochebrochard <enzo.delarochebrochard@ieva.io>
2020-02-08 13:41:28 +0100

    renamed server.js to app.js

--- app.js
+++ app.js
@@ -3,20 +3,32 @@ const app = express()
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const Movie = require('./api/models/movieListModel')
+const User = require('./api/models/UserModel')
 const bodyParser = require('body-parser')
+const routes = require('./api/routes/routes')
+
+const passport = require('passport')
+
+const config = require('./api/config/passport')
+
+
+const cors = require('cors')
+
+const movieCtrl = require('./api/controllers/movieListController')
 
 mongoose.Promise = global.Promise
-mongoose.connect( 'mongodb://localhost/Moviedb'/* , { useNewUrlParser: true, useUnifiedTopology: true } */)
+mongoose.connect( 'mongodb://localhost/Moviedb')
+
+app.use(passport.initialize())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// app.use((req, res) => {
-//     res.status(404).send({ url: `${req.originalUrl} not found` })
-// })
+app.use(cors())
 
-const routes = require('./api/routes/movieListRoute')
 routes(app)
 
 app.listen(port)
+
+// movieCtrl.createMovies()
 
 console.warn(`Movie API started on ${port}`)
